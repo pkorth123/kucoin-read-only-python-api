a130d5c292a28355658080439d96cec5ddab88d1
Patrick Korth <patrickkorth@Patricks-Mac-mini.local>
2021-12-14 19:21:14 -0500

    removed kucoin credentials before remote push

--- price_tracker.py
+++ price_tracker.py
@@ -1,9 +1,9 @@
 from kucoin.client import Client
 
 
-api_passphrase = 'SlackPack83'
-api_key  = '61b930a39927ba00018cf593'
-api_secret = 'c99f9126-12e5-4fa2-9d94-472df4d22f33'
+api_passphrase = ''
+api_key  = ''
+api_secret = ''
 client = Client(api_key, api_secret, api_passphrase)
 
 
