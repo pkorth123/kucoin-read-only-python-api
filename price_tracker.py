706cbaa54b4a17ffeb21444484b8a1082f4a7248
Patrick Korth <pkorth123@gmail.com>
2021-12-16 15:04:26 -0500

    added environmental variables for kucoin credentials

--- price_tracker.py
+++ price_tracker.py
@@ -1,14 +1,18 @@
+import os
+
 from kucoin.client import Client
 
 
-api_passphrase = ''
-api_key  = ''
-api_secret = ''
+api_passphrase = os.environ['KUCOIN_API_PASSPHRASE_READ_ONLY']
+api_key  = os.environ['KUCOIN_API_KEY_READ_ONLY']
+api_secret = os.environ['KUCOIN_API_SECRET_READ_ONLY']
 client = Client(api_key, api_secret, api_passphrase)
 
+currency = 'BTC-USDT'
 
 def main():
-    print(client.get_ticker('BTC-USDT')['price'])
+    print(client.get_24hr_stats(currency))
+    print(client.get_ticker(currency)['price'])
 
 if __name__ == "__main__":
     main()
