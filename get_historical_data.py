27b8f38a0112f3f040e4dc279ca517aca221ebef
Patrick Korth <pkorth123@gmail.com>
2021-12-19 18:04:46 -0500

    added script to fetch historical data from kucoin and plot it

---
+++ get_historical_data.py
@@ -0,0 +1,54 @@
+import requests
+from datetime import datetime
+from time import time
+import pandas as pd
+import matplotlib.pyplot as plt
+
+base_url = "https://api.kucoin.com"
+coin_pair = "BTC-USDT"  # BTC-USDT
+frequency = "1hour" #1day 4hour 1min
+# get timestamp date of today in seconds
+now_is = int(time())
+days = 40
+# sec  min  hour days
+days_delta = 60 * 60 * 24 * days
+start_At = now_is - days_delta
+# print(now_is)
+price_url = f"/api/v1/market/candles?type={frequency}&symbol={coin_pair}&startAt={start_At}&endAt={now_is}"
+price_dict = {}
+
+prices = requests.get(base_url + price_url).json()
+print(prices)
+
+for item in prices['data']:
+    # convert date from timestamp to Y M D
+    date_converted = datetime.fromtimestamp(int(item[0])).strftime("%Y-%m-%d-%H")
+    price_dict[date_converted] = item[2]
+
+priceDF = pd.DataFrame(price_dict, index=["price"]).T
+# Convert prices into a float
+priceDF['price'] = priceDF['price'].astype(float)
+
+# convert dates to datetime from object
+priceDF.index = pd.to_datetime(priceDF.index)
+
+# reverse dates
+priceDF = priceDF.iloc[::-1]
+print(priceDF)
+
+# moving_average 200 days(
+priceDF['200MA'] = priceDF['price'].rolling(200).mean()
+priceDF['52MA'] = priceDF['price'].rolling(52).mean()
+
+priceDF
+
+# plot
+
+fig, ax = plt.subplots()
+ax.plot(priceDF[['price', '200MA', '52MA']])
+# Rotate and align the tick labels so they look better.
+fig.autofmt_xdate()
+ax.legend(['price', '200MA', '52MA'])
+# Use a more precise date string for the x axis locations in the toolbar.
+
+plt.show()
--- price_tracker.py
+++ price_tracker.py
@@ -1,5 +1,5 @@
 import os
-
+import metatrader
 from kucoin.client import Client
 
 
@@ -10,9 +10,25 @@ client = Client(api_key, api_secret, api_passphrase)
 
 currency = 'BTC-USDT'
 
+
 def main():
-    print(client.get_24hr_stats(currency))
-    print(client.get_ticker(currency)['price'])
+    btc_daily_data_path = os.path.join(os.path.dirname(__file__), 'backtesting-data/btc_11-18-2020_12-18-2020.csv')
+    with open(btc_daily_data_path) as csv:
+        btc_daily_data = csv.readlines()
+    for day in btc_daily_data:
+        if day[2] == "D":
+            continue
+        else:
+            date = day.split('\",\"')[0]
+            price = day.split('\",\"')[1]
+            daily_open = day.split('\",\"')[2]
+            daily_high = day.split('\",\"')[3]
+            daily_low = day.split('\",\"')[4]
+            daily_volume = day.split('\",\"')[5]
+            daily_change_percent = day.split('\",\"')[6]
+    # print(client.get_24hr_stats(currency))
+    # print(client.get_ticker(currency)['price'])
+    # client.get_trade_histories(currency)
 
 if __name__ == "__main__":
     main()
