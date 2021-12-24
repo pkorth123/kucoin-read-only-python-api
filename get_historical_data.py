f1fda20dfeb6ae51ce64d52b0690d68f76357bee
Patrick Korth <pkorth123@gmail.com>
2021-12-24 15:33:53 -0500

    added functions to get donchian channels given a set of historical data, a period, and a timeframe

--- get_historical_data.py
+++ get_historical_data.py
@@ -4,51 +4,66 @@ from time import time
 import pandas as pd
 import matplotlib.pyplot as plt
 
-base_url = "https://api.kucoin.com"
-coin_pair = "BTC-USDT"  # BTC-USDT
-frequency = "1hour" #1day 4hour 1min
 # get timestamp date of today in seconds
 now_is = int(time())
-days = 40
 # sec  min  hour days
-days_delta = 60 * 60 * 24 * days
-start_At = now_is - days_delta
 # print(now_is)
-price_url = f"/api/v1/market/candles?type={frequency}&symbol={coin_pair}&startAt={start_At}&endAt={now_is}"
 price_dict = {}
 
-prices = requests.get(base_url + price_url).json()
-print(prices)
 
-for item in prices['data']:
-    # convert date from timestamp to Y M D
-    date_converted = datetime.fromtimestamp(int(item[0])).strftime("%Y-%m-%d-%H")
-    price_dict[date_converted] = item[2]
+# def main():
+#     coin_pair = "XRP-USDT"  # BTC-USDT
+#     time_frame = "1hour"  # 1day 4hour 1min
+#     days = 40
+#     price_dict = get_pair_history(time_frame, coin_pair, days)
+#     plot_price(price_dict)
 
-priceDF = pd.DataFrame(price_dict, index=["price"]).T
-# Convert prices into a float
-priceDF['price'] = priceDF['price'].astype(float)
 
-# convert dates to datetime from object
-priceDF.index = pd.to_datetime(priceDF.index)
+def get_pair_history(coin_pair, time_frame, days_back):
+    days_delta = 60 * 60 * 24 * days_back
+    start_At = now_is - days_delta
+    base_url = "https://api.kucoin.com"
+    price_url = f"/api/v1/market/candles?type={time_frame}&symbol={coin_pair}&startAt={start_At}&endAt={now_is}"
+    prices = requests.get(base_url + price_url).json()
+    for item in prices['data']:
+        # convert date from timestamp to Y M D
+        date_converted = datetime.fromtimestamp(int(item[0])).strftime("%Y-%m-%d-%H")
+        price_dict[date_converted] = item[0], item[1], item[2], item[3], item[4], item[5], item[6]
+    return price_dict
 
-# reverse dates
-priceDF = priceDF.iloc[::-1]
-print(priceDF)
 
-# moving_average 200 days(
-priceDF['200MA'] = priceDF['price'].rolling(200).mean()
-priceDF['52MA'] = priceDF['price'].rolling(52).mean()
+def plot_price(price_dict):
+    plot_dict = {}
+    for each_line in price_dict:
+        plot_dict[each_line] = price_dict[each_line][2]
+    priceDF = pd.DataFrame(plot_dict, index=["price"]).T
+    # Convert prices into a float
+    priceDF['price'] = priceDF['price'].astype(float)
 
-priceDF
+    # convert dates to datetime from object
+    priceDF.index = pd.to_datetime(priceDF.index)
 
-# plot
+    # reverse dates
+    priceDF = priceDF.iloc[::-1]
+    print(priceDF)
 
-fig, ax = plt.subplots()
-ax.plot(priceDF[['price', '200MA', '52MA']])
-# Rotate and align the tick labels so they look better.
-fig.autofmt_xdate()
-ax.legend(['price', '200MA', '52MA'])
-# Use a more precise date string for the x axis locations in the toolbar.
+    # moving_average 200 days(
+    priceDF['200MA'] = priceDF['price'].rolling(200).mean()
+    priceDF['52MA'] = priceDF['price'].rolling(52).mean()
 
-plt.show()
+    priceDF
+
+    # plot
+
+    fig, ax = plt.subplots()
+    ax.plot(priceDF[['price', '200MA', '52MA']])
+    # Rotate and align the tick labels so they look better.
+    fig.autofmt_xdate()
+    ax.legend(['price', '200MA', '52MA'])
+    # Use a more precise date string for the x axis locations in the toolbar.
+
+    plt.show()
+
+
+# if __name__ == "__main__":
+#     main()
--- price_tracker.py
+++ price_tracker.py
@@ -1,34 +1,72 @@
 import os
 import metatrader
 from kucoin.client import Client
+import queue
+
+from get_historical_data import get_pair_history, plot_price
 
 
-api_passphrase = os.environ['KUCOIN_API_PASSPHRASE_READ_ONLY']
-api_key  = os.environ['KUCOIN_API_KEY_READ_ONLY']
-api_secret = os.environ['KUCOIN_API_SECRET_READ_ONLY']
-client = Client(api_key, api_secret, api_passphrase)
-
-currency = 'BTC-USDT'
+# don't really need api access to account unless actively trading
+# api_passphrase = os.environ['KUCOIN_API_PASSPHRASE_READ_ONLY']
+# api_key  = os.environ['KUCOIN_API_KEY_READ_ONLY']
+# api_secret = os.environ['KUCOIN_API_SECRET_READ_ONLY']
+# client = Client(api_key, api_secret, api_passphrase)
 
 
 def main():
-    btc_daily_data_path = os.path.join(os.path.dirname(__file__), 'backtesting-data/btc_11-18-2020_12-18-2020.csv')
-    with open(btc_daily_data_path) as csv:
-        btc_daily_data = csv.readlines()
-    for day in btc_daily_data:
-        if day[2] == "D":
-            continue
-        else:
-            date = day.split('\",\"')[0]
-            price = day.split('\",\"')[1]
-            daily_open = day.split('\",\"')[2]
-            daily_high = day.split('\",\"')[3]
-            daily_low = day.split('\",\"')[4]
-            daily_volume = day.split('\",\"')[5]
-            daily_change_percent = day.split('\",\"')[6]
-    # print(client.get_24hr_stats(currency))
-    # print(client.get_ticker(currency)['price'])
-    # client.get_trade_histories(currency)
+    # '1hour', '1day', '4hour', or '1min'
+    time_frame = "1hour"
+    coin_pair = 'BTC-USDT'
+    days_back = 40
+    backtest_market(coin_pair, time_frame, days_back)
+
+
+def backtest_market(coin_pair, time_frame, days_back):
+    data = get_pair_history(coin_pair, time_frame, days_back)
+    backtest_donchian(data)
+    # plot_price(data)
+
+
+def backtest_donchian(data, period=20):
+    get_donchian_channels(data, period)
+
+
+def get_donchian_channels(data, period):
+    donchian_channel = []
+    highs_q = queue.Queue(period)
+    lows_q = queue.Queue(period)
+    i = 0
+    for candle in data:
+        i += 1
+        high = data[candle][3]
+        low = data[candle][4]
+        highs_q.put(high)
+        lows_q.put(low)
+        if highs_q.full():
+            highest_high, lowest_low = get_donchian_vals(highs_q, lows_q)
+            highs_q.get()
+            lows_q.get()
+            middle_channel = (highest_high + lowest_low)/2
+            donchian_channel.append((highest_high, lowest_low, middle_channel))
+    for donchian_vals in donchian_channel:
+        print(donchian_vals)
+
+
+# get donchian high channel, low channel and middle channel for a given period represented by queues of length {period}
+def get_donchian_vals(highs_q, lows_q):
+    # highs = list(highs_q)
+    # lows = list(lows_q)
+    highest_high = 0
+    lowest_low = 1000000
+    for i in range(len(highs_q.queue)):
+        high_val = float(highs_q.queue[i])
+        low_val = float(lows_q.queue[i])
+        if float(high_val > highest_high):
+            highest_high = high_val
+        if float(low_val < lowest_low):
+            lowest_low = low_val
+    return highest_high, lowest_low
+
 
 if __name__ == "__main__":
     main()
